3359b2f7f715ffd6c3a1955fffa04aeaa6e6e63d
Pavel Stránský <microsoft@pavelstransky.cz>
2021-03-16 13:37:33 +0100

    Příklad na grafy

---
+++ python/plot.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+import math
+
+# plt.ion()                             # Interactive Mode On - pokud spustíme tuto funkci, program nebude zastavovat na plt.show()
+
+xs = [0.1 * i for i in range(100)]      # Hodnoty x od 0 do 10 s krokem 0.1
+ys1 = [math.sin(x) for x in xs]         # Funkce sin(x)
+
+plt.plot(xs, ys1)                       # Vykreslení grafu
+plt.show()                              # Zobrazení grafu
+
+
+""" Více grafů v jednom panelu """
+ys2 = [math.tan(x) for x in xs]
+
+plt.plot(xs, ys1)
+plt.plot(xs, ys2)
+plt.show()
+
+""" Popisky os a grafu"""
+plt.plot(xs, ys1, color="red", linestyle=":", label="$\\sin(x)$")
+plt.plot(xs, ys2, "b-.", label="$\\tan(x)$")
+plt.xlabel("$x$")                       # Popisky os (lze použít příkazy LaTeXu uvozené znaky $$)
+plt.ylabel("$y$")
+plt.title("Goniometrie")                # Popisek grafu
+plt.ylim(-3, 3)                         # Meze osy y
+plt.legend()                            # Zobrazí legendu
+plt.savefig("d:\\obrazek.pdf")          # Uloží obrázek do souboru
+plt.show()
+
+""" Více panelů """
+plt.subplot(211)                        # Číslo vyjadřuje počet řádků, počet sloupců a pořadí grafu
+plt.plot(xs, ys1)
+plt.xlabel("$x$")
+plt.ylabel("$\\sin(x)$")
+
+plt.subplot(212)
+plt.plot(xs, ys2, color="red", linewidth=3.0)
+plt.xlabel("$x$")
+plt.ylabel("$\\tan(x)$")
+plt.ylim(-3, 3)
+
+plt.suptitle("Goniometrie")             # Hlavní nadpis celého grafu
+plt.show()
