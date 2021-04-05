9008e7c3d3415b7716b5b5935f86d301e639ce07
Pavel Stránský <pcfyzika@pavelstransky.cz>
2021-04-05 15:05:24 +0200

    4. kapitola - řešení úloh

--- python/ode/ho.py
+++ python/ode/ho.py
@@ -24,7 +24,7 @@ graphs.plot_compare_methods(ho, (0, 1), index=0, analytical_solution=ho_solution
 graphs.plot_compare_methods(ho, (0, 1), index=0, analytical_solution=ho_solution, integrators=[ode.euler_2, ode.runge_kutta_4, symplectic.verlet, symplectic.euler_1_modified_x], dt=dt, maxt=maxt)
 graphs.plot_compare_steps(ho, (0, 1), ode.euler_1, analytical_solution=ho_solution, maxt=maxt)
 
-symplectic.plot_energy(ho, (0, 1), energy_function=ho_energy, dt=dt, maxt=maxt)
-symplectic.plot_energy(ho, (0, 1), energy_function=ho_energy, dt=dt, maxt=maxt, integrators=[ode.euler_2, ode.runge_kutta_4, symplectic.verlet])
+symplectic.plot_energy(ho, (0, 1), energy_function=ho_energy, dt=dt, maxt=maxt, integrators=[ode.euler_1, ode.euler_2, ode.runge_kutta_4])
+symplectic.plot_energy(ho, (0, 1), energy_function=ho_energy, dt=dt, maxt=maxt, integrators=[ode.euler_2, ode.runge_kutta_4, symplectic.verlet, symplectic.euler_1_modified_x])
 
 graphs.plot_cummulative_error(ho, (0, 1), ho_solution, index=0, integrators=[ode.euler_1, ode.euler_2, ode.runge_kutta_4, symplectic.verlet, symplectic.euler_1_modified_x])
--- python/ode/lorenz.py
+++ python/ode/lorenz.py
@@ -23,4 +23,14 @@ plt.figure()
 plt.plot(ys[:,0], ys[:,2])
 plt.xlabel("x")
 plt.ylabel("z")
+plt.title("Lorenz attractor")
 plt.show()
+
+# Thanks Samuel J. for this part of the code (plots a 3D interactive graph)
+try:                                                                                            
+    import plotly.express as px                                                                 
+    
+    fig = px.line_3d(x=ys[:,0], y=ys[:,1], z=ys[:,2])
+    fig.show()
+except:
+    print("Missing Plotly module.")    
