1dae95109997aab75a2f5177d678e7f557504685
Pavel Stránský <pcfyzika@pavelstransky.cz>
2021-04-06 09:50:36 +0200

    Náhodná procházka - řešení

---
+++ python/randomwalk/2d.py
@@ -0,0 +1,87 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# random.seed(0)                                            # For always the same walk uncomment this
+
+def random_direction_2d():
+    """ Generates a random direction in the 2D plane """
+    phi = random.uniform(0, 2 * np.pi)
+    return np.array([np.cos(phi), np.sin(phi)])
+    
+
+def random_walk_2d(num_steps=1000, step_size=1, box_size=100, initial_condition=[0,0]):
+    """ Generates and plots a random walk in a 2D plane.
+        If the trajectory leaves the given box, the calculation is interrupted.
+    """    
+    position = np.array(initial_condition)
+    path = [position]
+
+    box = np.array([box_size, box_size])
+
+    for _ in range(0, num_steps):                           # _ means unused variable
+        position = position + step_size * random_direction_2d() # for some unknown reason the command
+                                                            # position += random_direction_2d()
+                                                            # doesn't work here
+
+        if (position < -box).any() or (position > box).any(): # A check whether we leave the given box
+            break
+
+        path.append(position)
+
+    path = np.array(path)
+
+    plt.ioff()
+    plt.plot(path[:,0], path[:,1], label=f"N={len(path)}")
+    plt.xlim(-box_size, box_size)
+    plt.ylim(-box_size, box_size)
+    plt.legend()
+    plt.show()
+
+    return path
+
+
+def random_walk_2d_interactive(num_steps=1000, step_size=1, box_size=100, initial_condition=[0,0], cyclic=False):
+    """ Generates and interactivelly plots a random walk in a 2D plane.
+    """    
+    position = np.array(initial_condition)
+    path = [position]
+
+    box = np.array([box_size, box_size]) 
+
+    # Switch on the interactive mode (in order to plot more random walks into one graph)
+    # - In Spyder you have to write the command "%matplotlib auto" into the REPL first
+    plt.ion()
+    plt.xlim(-box_size, box_size)
+    plt.ylim(-box_size, box_size)
+    
+    for i in range(0, num_steps):
+        new_position = position + step_size * random_direction_2d()            
+
+        if (new_position < -box).any() or (new_position > box).any(): # A check whether we are within the given box
+            if not cyclic:                              # Not cyclic boundary conditions - leave the for cycle
+                break
+
+            shift_new_position = (new_position + box) % (2 * box) - box
+
+            ### This part of the code is just for aesthetic reasons
+            shift_position = position + (shift_new_position - new_position)
+            plt.plot([position[0], new_position[0]], [position[1], new_position[1]], 'blue')
+            plt.plot([shift_position[0], shift_new_position[0]], [shift_position[1], shift_new_position[1]], 'blue')
+            ###
+
+            new_position = shift_new_position
+
+        else:                                           # Plot only if we don't apply the cyclic boundary conditions
+            plt.plot([position[0], new_position[0]], [position[1], new_position[1]], 'blue')
+            plt.title(i)
+            plt.show()
+            plt.pause(0.1)
+
+        path.append(new_position)
+        position = new_position
+
+    path = np.array(path)
+    return path
+
+random_walk_2d_interactive(step_size=10)
---
+++ python/randomwalk/nd.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+generator = np.random.default_rng()
+#generator.seed(0)
+
+def random_direction(dimension):
+    """ Generates random direction in the dimension-dimensional space by generating a point
+        in a unit cube and deciding whether it lies inside a unit sphere.
+        In higher dimension it is highly ineffective because a majority of points lie outside
+        of the sphere.
+    """
+    while True:                                 # This should be used with care - infinite cycle
+        random_vector = 2 * generator.random(dimension) - 1
+        norm = np.linalg.norm(random_vector)    # Norm of the vector
+        if norm <= 1:                           # We are within the unit cube
+            return random_vector / norm
+        
+
+def random_direction_gaussian(dimension):
+    """ Generates random direction in the dimension-dimensional space 
+
+        References:
+        [1] M.E. Muller, A note on a method for generating points uniformly on n-dimensional spheres,
+                Communications of the Asociation for Computing Machinery 2, 19 (1959)
+        [2] G. Marsaglia, Choosing a Point from the Surface of a Sphere,
+                The Annals of Mathematical Statistics 43, 645 (1972)
+    """
+    random_vector_gaussian = generator.normal(size=dimension)
+    return random_vector_gaussian / np.linalg.norm(random_vector_gaussian)
+    
+
+def random_walk(dimension=3, num_steps=1000, step_size=1, box_size=100, initial_condition=0, direction=random_direction, cyclic=False):
+    """ Generates and plots a random walk in an arbitrary dimension.
+        If the trajectory leaves the given box, the calculation is interrupted.
+    """
+    if initial_condition == 0:                   # We start from the origin
+        initial_condition = np.zeros(dimension)
+    
+    position = np.array(initial_condition)
+    path = [position]
+
+    box = np.full(dimension, box_size)          # Creates an array with size=dimension whose each element has value box_size                                            
+
+    for _ in range(0, num_steps):
+        new_position = position + step_size * direction(dimension)
+
+        if (new_position < -box).any() or (new_position > box).any(): # A check whether we are within the given box
+            if not cyclic:                      # Not cyclic boundary conditions - leave the for cycle
+                break
+
+            new_position = (new_position + box) % (2 * box) - box
+        
+        path.append(new_position)
+        position = new_position
+
+    path = np.array(path)
+    return path
+
+print(random_walk(dimension=10, num_steps=100))
