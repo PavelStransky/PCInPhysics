bf9dea5c017337af31b7da24f983cab4e3c19bff
Pavel Stránský <pcfyzika@pavelstransky.cz>
2021-05-17 19:46:19 +0200

    Fourierova transformace

---
+++ python/basics/sound.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sounddevice as sd
+import soundfile as sf
+import matplotlib.pyplot as plt
+
+import time
+
+def Read(file):
+    sound, fs = sf.read(file, dtype='float32')
+    return sound, fs
+
+def Play(sound, fs):
+    sd.play(sound, fs)
+    sd.wait()
+
+if __name__ == "__main__":
+    Play(*Read("../../sounds/a.wav"))
