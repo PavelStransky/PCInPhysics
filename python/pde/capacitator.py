8780d0ed49a537a6db1a130e4aac6cb5abb76202
Pavel Stránský <github@pavelstransky.cz>
2023-03-16 17:11:35 +0100

    PDR - Laplace ve 2D

---
+++ python/pde/capacitator.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm                       # Colour maps for the contour graph
+
+import poisson
+
+lengthx = 50                                    # Capacitator length
+lengthy = 50                                    # Distance between plates - horizontal axis
+
+initial = np.full((lengthx, lengthy), -1.0)
+initial[:, 0] = 0                               # Bottom layer (ground)
+initial[:, -1] = 10                             # Top layer
+
+rod_index = 25
+rod_length = 30
+initial[rod_index, 0:rod_length] = 0            # Lightning rod
+
+person_index = 20
+person_length = 10
+initial[person_index, 0:person_index] = 0       # Person
+
+potential = poisson.solve_2d(initial, mask=-1.0, iterations=5000)
+
+plt.pcolormesh(np.transpose(potential), cmap=cm.hsv)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.colorbar(label=r"$\varphi$")                # Legend for the contour graph
+plt.show()
+
+plt.plot(potential[rod_index, :], label="Hromosvod")
+plt.plot(potential[person_index, :], label="Člověk")
+plt.xlabel("y")
+plt.ylabel(r"$\varphi$")
+plt.legend()
+plt.show()
+
+charge = poisson.charge_density(potential)
+
+plt.pcolormesh(np.transpose(charge), cmap=cm.hsv) 
+plt.xlabel("x")
+plt.ylabel("y")
+plt.colorbar(label=r"$\rho$")
+plt.show()
+
+# Charge on the top of the lightning rod
+plt.plot(charge[rod_index, :], label=f"Špička hromosvodu: {charge[rod_index, rod_length - 1]}")
+plt.plot(charge[person_index, :], label=f"Hlava člověka: {charge[person_index, person_length - 1]}")
+plt.xlabel("y")
+plt.ylabel(r"$\rho$")
+plt.legend()
+plt.show()
---
+++ python/pde/poisson.py
@@ -0,0 +1,76 @@
+import numpy as np
+def solve_2d(initial, mask=0, iterations=2000):   
+    """ Calculates the Laplace equation in 2D with a Dirichlet boundary condition using
+        the Gauss-Seidel method.
+        
+        Arguments:
+        initial -- Initial matrix; elements without fixed boudary condition have value mask
+        mask -- Special value marking elements where we look for the solution
+        iterations -- Number of iterations
+    """
+    potential = np.copy(initial)
+    potential[potential == mask] = 0
+
+    lengthx, lengthy = initial.shape
+
+    for _ in range(iterations):
+        for i in range(lengthx):
+            for j in range(lengthy):
+                if initial[i, j] == mask:
+                    v = 0
+                    n = 0
+                    if i > 0:
+                        v += potential[i - 1, j]
+                        n += 1
+                    if i < lengthx - 1:
+                        v += potential[i + 1, j]
+                        n += 1
+                    if j > 0:
+                        v += potential[i, j - 1]
+                        n += 1
+                    if j < lengthy - 1:
+                        v += potential[i, j + 1]
+                        n += 1
+                    potential[i, j] = v / n
+        
+    return potential
+
+def charge_density(potential):
+    """ Returns the charge density connected with the given 2D potential """
+    result = np.zeros_like(potential)
+
+    lengthx, lengthy = potential.shape
+
+    for i in range(lengthx):
+        for j in range(lengthy):
+            v = 0
+            if i > 0:
+                v += potential[i - 1, j]
+                v -= potential[i, j]
+            if i < lengthx - 1:
+                v += potential[i + 1, j]
+                v -= potential[i, j]
+            if j > 0:
+                v += potential[i, j - 1]
+                v -= potential[i, j]
+            if j < lengthy - 1:
+                v += potential[i, j + 1]
+                v -= potential[i, j]
+
+            result[i, j] = v
+        
+    return result
+
+def force(potential):
+    """ Returns the components of the electrostatical force connected with the potential """
+    forcex = np.zeros_like(potential)
+    forcey = np.zeros_like(potential)
+
+    lengthx, lengthy = potential.shape
+
+    for i in range(1, lengthx - 1):
+        for j in range(1, lengthy - 1):
+            forcex[i, j] = -0.5 * (potential[i + 1, j] - potential[i - 1, j])
+            forcey[i, j] = -0.5 * (potential[i, j + 1] - potential[i, j - 1])
+
+    return forcex, forcey
