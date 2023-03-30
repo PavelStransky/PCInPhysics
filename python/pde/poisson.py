b2203f26f82a3f605d4f5c34c4f1d04ba19fe18d
Pavel Stránský <github@pavelstransky.cz>
2023-03-30 14:23:17 +0200

    Periodické okrajové podmínky v x

--- python/pde/poisson.py
+++ python/pde/poisson.py
@@ -35,6 +35,42 @@ def solve_2d(initial, mask=0, iterations=2000):
         
     return potential
 
+def solve_2d_periodic_x(initial, mask=0, iterations=2000):   
+    """ Calculates the Laplace equation in 2D with a Dirichlet boundary condition using
+        the Gauss-Seidel method. Periodic boundary in x.
+        
+        Arguments:
+        initial -- Initial matrix; elements without fixed boudary condition have value mask
+        mask -- Special value marking elements where we look for the solution
+        iterations -- Number of iterations
+    """
+    potential = np.copy(initial)
+    potential[potential == mask] = 0
+
+    lengthx, lengthy = initial.shape
+
+    for _ in range(iterations):
+        for i in range(lengthx):
+            for j in range(lengthy):
+                if initial[i, j] == mask:
+                    v = 0
+                    n = 0
+
+                    v += potential[i - 1, j]
+                    v += potential[(i + 1) % lengthx, j]
+                    n += 2
+
+                    if j > 0:
+                        v += potential[i, j - 1]
+                        n += 1
+                    if j < lengthy - 1:
+                        v += potential[i, j + 1]
+                        n += 1
+
+                    potential[i, j] = v / n
+        
+    return potential
+
 def charge_density(potential):
     """ Returns the charge density connected with the given 2D potential """
     result = np.zeros_like(potential)
@@ -68,9 +104,9 @@ def force(potential):
 
     lengthx, lengthy = potential.shape
 
-    for i in range(1, lengthx - 1):
+    for i in range(lengthx):
         for j in range(1, lengthy - 1):
-            forcex[i, j] = -0.5 * (potential[i + 1, j] - potential[i - 1, j])
+            forcex[i, j] = -0.5 * (potential[(i + 1) % lengthx, j] - potential[i - 1, j])
             forcey[i, j] = -0.5 * (potential[i, j + 1] - potential[i, j - 1])
 
     return forcex, forcey
