ba7c77e361feadb56ce5c39b305ad2ad46c7dfc7
Pavel Stránský <github@pavelstransky.cz>
2023-04-28 11:36:22 +0200

    Drobné úpravy multiprocessing

--- python/montecarlo/parallelization.py
+++ python/montecarlo/parallelization.py
@@ -3,42 +3,49 @@ import matplotlib.pyplot as plt
 
 import time
 
-from multiprocessing import Pool, Process, Value
+import multiprocessing as mp
 from integration import *
 
 
 def integrate_1D_Pool(p, n, function, a, b):
-    """ Using Pool and map to perform the Monte-Carlo integration """
-    with Pool(processes=p) as pool:
-        args = ((n, function, a, b),) * p
+    """ Using Pool and map to perform the Monte-Carlo integration 
+        
+        Parameters: 
+        p: Number of processes
+        n: Total number of Monte-Carlo points
+        function: Integrated function
+        a, b: Integration limits
+    """
+    n1 = n // p             # Number of MC points calculated by each process
+
+    with mp.Pool(processes=p) as pool:
+        args = ((n1, function, a, b),) * p
         # Copies p-times the given n-tuple. The same as 
         # args = [(n, function, a, b) for _ in range(p)]
+        
         results = pool.starmap(integrate_1D, args)
+        #results = pool.starmap_async(integrate_1D, args).get() # Asynchronous procesing
 
-    average = sum(results) / len(results)
-    return average
+    result = sum(results) / len(results)
+    return result
 
 
-def plot_1D_duration(processes=range(1, 17), n=1000000, function=f1, a=0, b=2*np.pi):
+def plot_1D_duration(processes=range(1, 25), n=1000000, function=f1, a=0, b=2*np.pi):
+    """ Plots a graph with the total computation time depending on the number of processes """
+
     def duration(p):
-        startTime = time.time()
+        start_time = time.time()
         result = integrate_1D_Pool(p, n, function, a, b)
-        duration = time.time() - startTime
+        duration = time.time() - start_time
         print(f"I({function.__name__}) = {result} (Doba výpočtu v {p} vláknech: {duration})")
         return duration
 
     durations = [duration(p) for p in processes]
     
     plt.plot(processes, durations)
-    plt.title("Celkový čas výpočtu")
+    plt.title(f"Celkový čas výpočtu pro N={n}")
     plt.xlabel(r"$p$")
     plt.ylabel(r"$T [s]$")
-    plt.show()
-
-    plt.plot(processes, np.array(durations) / np.array(processes))
-    plt.title("Čas výpočtu na vlákno")
-    plt.xlabel(r"$p$")
-    plt.ylabel(r"$t [s]$")
     plt.show()
 
 
@@ -48,13 +55,13 @@ def integrate_1D_parallel(result, *args, **kwargs):
 
 
 def integrate_1D_Process(p, n, function, a, b):
-    """ Using Process to perform Monte-Carlo integration """
+    """ Using Process for Monte-Carlo integration """
     processes = []
     results = []
 
     for _ in range(p):
-        result = Value('d', 0)
-        process = Process(target=integrate_1D_parallel, args=(result, n, function, a, b))
+        result = mp.Value('d', 0)
+        process = mp.Process(target=integrate_1D_parallel, args=(result, n, function, a, b))
         process.start()
         processes.append(process)
         results.append(result)
@@ -67,6 +74,9 @@ def integrate_1D_Process(p, n, function, a, b):
 
 
 if __name__ == "__main__":
+    print("Počet logických jader:", mp.cpu_count())
+
     print(integrate_1D_Pool(8, 100000, f1, 0, 2 * np.pi))
-    plot_1D_duration()
+    plot_1D_duration(n=1000000)
+    plot_1D_duration(n=10000000)
     print(integrate_1D_Process(8, 100000, f1, 0, 2 * np.pi))
